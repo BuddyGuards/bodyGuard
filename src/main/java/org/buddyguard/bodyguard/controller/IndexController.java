7a5ef4b41b787a9cb35d8415f8c0b65d0b7900df
Boaz11Jachin <sheepo84k@gmail.com>
2025-04-11 18:56:10 +0900

    chore : 헬스체크 2

--- src/main/java/org/buddyguard/bodyguard/controller/IndexController.java
+++ src/main/java/org/buddyguard/bodyguard/controller/IndexController.java
@@ -1,17 +1,9 @@
 package org.buddyguard.bodyguard.controller;
 
+
 import lombok.AllArgsConstructor;
-import org.codenova.moneylog.entity.User;
-import org.codenova.moneylog.repository.ExpenseRepository;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
-
-import java.time.LocalDate;
-import java.util.Optional;
-
 
 @Controller
 @AllArgsConstructor
