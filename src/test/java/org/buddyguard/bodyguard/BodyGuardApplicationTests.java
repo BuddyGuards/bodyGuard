bdd8304a0eacb1603108655bc81d65da48eb29da
Boaz11Jachin <sheepo84k@gmail.com>
2025-04-11 18:42:28 +0900

    chore : 헬스체크

---
+++ src/test/java/org/buddyguard/bodyguard/BodyGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.buddyguard.bodyguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodyGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
