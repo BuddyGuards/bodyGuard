be5b1d7319394ba095a02cf060b6fed7f64afe6e
Boaz11Jachin <sheepo84k@gmail.com>
2025-04-13 15:54:12 +0900

    feat : 회원관리기능(회원가입, 로그인, 로그아웃) 구현

---
+++ src/main/java/org/buddyguard/bodyguard/entity/User.java
@@ -0,0 +1,30 @@
+package org.buddyguard.bodyguard.entity;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private int id;
+    private String email;
+    private String password;
+    private String nickname;
+    private String imageUrl;
+    private String provider;
+    private String providerId;
+    private String verified;
+    private LocalDateTime createdAt;
+
+    private String infoPublic;
+    private String gender;
+    private double height;
+    private double weight;
+    private int age;
+    private String goal;
+}
