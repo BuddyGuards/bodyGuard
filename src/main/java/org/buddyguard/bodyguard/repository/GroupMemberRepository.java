649808601243849feff2ed9a0a84e5123fff971c
Boaz11Jachin <sheepo84k@gmail.com>
2025-04-14 20:01:32 +0900

    feat : 모임 만들기기능 0.0.2 작업중 / 남성여성도 수정중

--- src/main/java/org/buddyguard/bodyguard/repository/GroupMemberRepository.java
+++ src/main/java/org/buddyguard/bodyguard/repository/GroupMemberRepository.java
@@ -2,8 +2,9 @@ package org.buddyguard.bodyguard.repository;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.buddyguard.bodyguard.entity.Group;
+import org.buddyguard.bodyguard.entity.GroupMember;
 
 @Mapper
 public interface GroupMemberRepository {
-
+    public int createApproved (GroupMember groupMember);
 }
