1e89e9097ccb189c2c52a72c110d0a42ab43ea5b
Boaz11Jachin <sheepo84k@gmail.com>
2025-04-14 17:43:50 +0900

    feat : 모임 만들기기능 0.0.1 작업중

---
+++ src/main/java/org/buddyguard/bodyguard/repository/GroupMemberRepository.java
@@ -0,0 +1,9 @@
+package org.buddyguard.bodyguard.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.buddyguard.bodyguard.entity.Group;
+
+@Mapper
+public interface GroupMemberRepository {
+
+}
