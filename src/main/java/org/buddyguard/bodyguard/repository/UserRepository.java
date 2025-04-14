256c0ecac200f8ff8056aca1d707363ac00d2d85
rin <krin02@naver.com>
2025-04-14 17:37:19 +0900

    feat : 마이페이지(프로필, 수정) 구현

--- src/main/java/org/buddyguard/bodyguard/repository/UserRepository.java
+++ src/main/java/org/buddyguard/bodyguard/repository/UserRepository.java
@@ -3,12 +3,16 @@ package org.buddyguard.bodyguard.repository;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.buddyguard.bodyguard.entity.User;
+import org.buddyguard.bodyguard.request.ProfileUpdateRequest;
 
 
 @Mapper
 public interface UserRepository {
 
-    public int save (User user);
+    public int save(User user);
+
     public User findByEmail(String email);
 
+    public void updateUserProfileByEmail(@Param("email") String email,
+                                         @Param("request") ProfileUpdateRequest request);
 }
