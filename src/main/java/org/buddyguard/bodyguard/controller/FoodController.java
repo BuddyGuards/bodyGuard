8a936ada3736d6c96da347172a90f6f84f619983
rin <krin02@naver.com>
2025-04-16 13:09:21 +0900

    feat : 푸드 다이어리 기초 페이지

---
+++ src/main/java/org/buddyguard/bodyguard/controller/FoodController.java
@@ -0,0 +1,17 @@
+package org.buddyguard.bodyguard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/food")
+
+public class FoodController {
+
+    @GetMapping("/diary")
+    public String showDiaryPage(Model model) {
+
+        return "food/diary";  // Thymeleaf 템플릿
+    }
+}
