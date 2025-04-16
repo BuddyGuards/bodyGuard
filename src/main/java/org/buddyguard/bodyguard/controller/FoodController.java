e6c053270e58eaaf8b9308b64cc52f468d5f7e8b
Boaz11Jachin <sheepo84k@gmail.com>
2025-04-16 18:29:48 +0900

    feat : food api 작업 중 0.1.0

--- src/main/java/org/buddyguard/bodyguard/controller/FoodController.java
+++ src/main/java/org/buddyguard/bodyguard/controller/FoodController.java
@@ -1,17 +1,39 @@
 package org.buddyguard.bodyguard.controller;
 
+import lombok.AllArgsConstructor;
+import org.buddyguard.bodyguard.entity.FoodLog;
+import org.buddyguard.bodyguard.entity.User;
+import org.buddyguard.bodyguard.repository.FoodLogRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/food")
-
+@AllArgsConstructor
 public class FoodController {
+
+    private FoodLogRepository foodLogRepository;
 
     @GetMapping("/diary")
     public String diaryHandle(Model model) {
 
         return "food/diary";  // Thymeleaf 템플릿
     }
+
+    @PostMapping("/diary")
+    public String diaryPostHandle (@ModelAttribute FoodLog foodLog,
+                                   @SessionAttribute("user") User user){
+
+        FoodLog fl = FoodLog.builder().userId(user.getId())
+                .foodId(foodLog.getFoodId())
+                .amount(foodLog.getAmount())
+                .eatenDate(foodLog.getEatenDate())
+                .mealType(foodLog.getMealType()).build();
+
+        foodLogRepository.save(fl);
+
+        return "redirect:/food/diary";
+    }
+
 }
